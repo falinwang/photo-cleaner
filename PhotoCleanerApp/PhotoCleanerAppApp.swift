48c2fe50475ee7a0e112a9c85b07d7f0d39045d4
royfwang <royflw@gmail.com>
2026-05-25 00:36:31 -0700

    Initial Commit

---
+++ PhotoCleanerApp/PhotoCleanerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanerAppApp.swift
+//  PhotoCleanerApp
+//
+//  Created by ROYWANG62 on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCleanerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
